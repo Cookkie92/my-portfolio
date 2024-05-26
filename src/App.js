bcec1756a738e78cd897fbeba4b75a6bbc38754d
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2024-05-26 21:05:21 +0200

    update.03

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 // src/App.js
-import React from 'react';
-import ProjectCard from './components/ProjectCard';
-import './App.css';
+import React from "react";
+import ProjectCard from "./components/ProjectCard";
+import "./App.css";
 
 const projects = [
   {
-    title: 'Semester Project 2',
-    description: 'A web application that manages tasks for a team.',
-    screenshot: require('./assets/images/semester project 2.PNG'),
-    repoLink: 'https://github.com/yourusername/semester-project-2',
-    liveLink: 'link_to_live_site'
+    title: "Semester Project 2",
+    description:
+      "A bidding website made using noroff api, here a user with noroff domain mail can register and login to post bids, and bid on others bids. user starts with 1000 points, but can earn more on their account when bids are complete. ",
+    screenshot: require("./assets/images/semester project 2.PNG"),
+    repoLink: "https://github.com/Cookkie92/Semester-Project-2",
+    liveLink: "https://candid-marshmallow-290178.netlify.app/",
   },
   {
-    title: 'JavaScript Frameworks CA',
-    description: 'A comprehensive web application built with React.',
-    screenshot: require('./assets/images/jsframeworkca.PNG'),
-    repoLink: 'https://github.com/yourusername/javascript-frameworks-ca',
-    liveLink: 'link_to_live_site'
+    title: "JavaScript Frameworks CA",
+    description:
+      "An online shop website where people can view product and add to cart, then checkout with their items, or delete if they dont want it.",
+    screenshot: require("./assets/images/jsframeworkca.PNG"),
+    repoLink: "https://github.com/Cookkie92/js-frameworks-ca",
+    liveLink: "https://incomparable-bavarois-077734.netlify.app/",
   },
   {
-    title: 'Project Exam 2',
-    description: 'A detailed project showcasing advanced JavaScript techniques.',
-    // screenshot: require('./assets/images/project-exam-2.PNG'),
-    repoLink: 'https://github.com/yourusername/project-exam-2',
-    liveLink: 'link_to_live_site'
-  }
+    title: "Project Exam 2",
+    description:
+      "A social media app that users using noroff domain can register user and post text and images.",
+    screenshot: require("./assets/images/socialmediaapp.PNG"),
+    repoLink: "https://github.com/Cookkie92/project-exam-2-cookkie92",
+    liveLink: "https://cookkie92.github.io/project-exam-2-cookkie92/",
+  },
 ];
 
 const App = () => {
